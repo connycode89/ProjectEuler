5c017cf2f91975337607ccf50ac0b2b1cf379762
connycode89 <32690940+connycode89@users.noreply.github.com>
2018-06-20 01:47:07 +0100

    Starting #24

---
+++ problem_24.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 20 01:41:36 2018
+
+@author: Conor
+"""
+
+# Python 2.7
+# https://projecteuler.net/problem=24
+
+def permute_n(n):
+    
+    
+permute(2)
